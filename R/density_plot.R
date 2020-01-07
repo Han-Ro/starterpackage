a9f10ca5ecab3e9f01ced264ad7589adf91fab5f
Hannah <hrc540@york.ac.uk>
2020-01-07 14:29:04 +0000

    Importing functions as a separate file to apply to whole package. Density plot example

--- R/density_plot.R
+++ R/density_plot.R
@@ -1,11 +1,10 @@
 #' Creating a density plot
 #'
-#' @param v
+#' @param v numeric vector
 #'
 #' @return graph
 #' @export
-#'@importFrom ggplot2 ggplot aes geom_density
-#'@importFrom magrittr %>%
+
 #' @examples
 density_plot <- function(v){
   stopifnot(is.numeric(v))
---
+++ R/imports.R
@@ -0,0 +1,6 @@
+# Imports for the package starterpackage
+#'@importFrom ggplot2 ggplot aes geom_density
+#'@importFrom magrittr %>%
+#'@importFrom stats sd
+#'
+NULL
