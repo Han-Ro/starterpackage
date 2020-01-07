011808b5f4d0caae60fa2346ec548e984592e841
Hannah <hrc540@york.ac.uk>
2020-01-07 15:07:27 +0000

    testing

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(starterpackage)
+
+test_check("starterpackage")
---
+++ tests/testthat/test-density_plot.R
@@ -0,0 +1,12 @@
+test_vec <- rnorm(20, mean = 5, sd = 1.2)
+
+
+
+
+
+
+
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
